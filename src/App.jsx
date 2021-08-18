7358e37a778fc23bf5be1ec52a0258183331650c
GerardoZuno <gerardozunounitec@gmail.com>
2021-08-18 16:17:04 -0500

    Add Header Component

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <Header titulo="Buscador de Noticias"/>
+      <div className="container white">
+        <h1>Formulario Aqui</h1>
+
+      </div>
+    </>
+  )
+}
+
+export default App
+
